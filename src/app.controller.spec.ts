6667da314d3a4df1e2a1141337128ca6fa87b858
Nicacioneto <nicacionetobsb@gmail.com>
2021-09-03 11:35:25 -0300

    Removing cats adding RabbitMQ

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hi! Welcome to core-ms!"', () => {
+      expect(appController.health()).toBe('Hi! Welcome to core-ms!');
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:27017/nest'), CatsModule],
+  imports: [MongooseModule.forRoot('mongodb://mongo:27017/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
